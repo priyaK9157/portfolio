a2e5fb77c49ce84af1019bb68818148c5c79d761
priyaK9157 <priyakumari2003004@gmail.com>
2024-06-14 18:53:34 +0530

    first commit

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,7 @@
 // @flow strict
 
 import { personalData } from "@/utils/data/personal-data";
-import BlogCard from "../components/homepage/blog/blog-card";
+// import BlogCard from "../components/homepage/blog/blog-card";
 
 async function getBlogs() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -29,14 +29,14 @@ async function page() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
         {
           blogs.map((blog, i) => (
             blog?.cover_image &&
             <BlogCard blog={blog} key={i} />
           ))
         }
-      </div>
+      </div> */}
     </div>
   );
 };
